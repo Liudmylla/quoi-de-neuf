895c6344964fceb093ed551d2a886081ae0f90d3
luda <liudmyla12@yahoo.fr>
2021-05-10 16:54:45 +0200

    Pagination added to home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,22 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Annonce;
 use App\Repository\AnnonceRepository;
 use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface; 
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(AnnonceRepository $annonceRepository, CategoryRepository $categoryRepository): Response
+    public function index(Request $request, PaginatorInterface $paginator,AnnonceRepository $annonceRepository, CategoryRepository $categoryRepository): Response
     {
+
+        $donnees = $this->getDoctrine()->getRepository(Annonce::class)->findBy([],['created_date' => 'desc']);
+
+        $annonces = $paginator->paginate(
+            $donnees, // Requête contenant les données à paginer (ici nos articles)
+            $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+            6 // Nombre de résultats par page
+        );
+        
         return $this->render('home/index.html.twig', [
-            'annonces' => $annonceRepository->findBy([], ['id' => 'DESC']),
+            'annonces' => $annonces,
             'categories' => $categoryRepository->findBy([], ['id' => 'ASC']),
         ]);
     }
+      
 }
